5f1219046c9b788bfbcf1f6b76e6cf0efd881cbb
Zniniz <80466823+Zniniz@users.noreply.github.com>
2023-10-10 16:06:47 -0400

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -8,37 +8,37 @@ while True:
     print("2. Département de production.(P)")
     print("3. Sortir du programme.(E)")
 
-    choix = char(input("Veuillez faire le choix de votre département.\n"))
+    choix = str(input("Veuillez faire le choix de votre département.\n"))
 
-    if choix == 'C':
+    if choix == "C":
         print("1. Représentants")
         print("2. Vendeurs")
         print("3. Sortir du programme.(E)")
-        choix2 = char(input("Veuillez faire le choix de votre employé.\n"))
-        if choix2 == 'R':
+        choix2 = str(input("Veuillez faire le choix de votre employé.\n"))
+        if choix2 == "R":
             r = int(input("Veuillez inserer le chiffre d'affaire realiser mensuellement."))
             print("Le salaire est: ", (r*rep)+1400,"$\n")
-        if choix2 == 'V':
+        if choix2 == "V":
             r = int(input("Veuillez inserer le chiffre d'affaire realise mensuellement."))
             print("Le salaire est: ", (r*vend)+1000,"$\n")
-        elif choix == 'E' or choix2 == 'E':
+        elif choix == "E" or choix2 == "E":
             exit()
 
-    elif choix == 'P':
+    elif choix == "P":
         print("1. Producteurs")
         print("2. Affectes a la manutention")
         print("3. Sortir du programme.(E)")
-        choix2 = char(input("Veuillez faire le choix de votre employé.\n"))
-        if choix2 == 'P':
+        choix2 = str(input("Veuillez faire le choix de votre employé.\n"))
+        if choix2 == "P":
             r = int(input("Veuillez inserer le nombre d'unites produits mensuellement."))
             print("Le salaire est: ", (r * prod), "$\n")
-        if choix2 == 'M':
+        if choix2 == "M":
             r = int(input("Veuillez inserer le nombre d'heure de travail mensuel."))
             print("Le salaire est: ", (r * manu), "$\n")
-        elif choix == 'E' or choix2 == 'E':
+        elif choix == "E" or choix2 == "E":
             exit()
 
-    elif choix == 'E':
+    elif choix == "E":
         exit()
     else:
         print("Le choix",choix,"est invalid, veuillez reessayer. \n")
